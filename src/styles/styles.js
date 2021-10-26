1fa4a6dd9cfc8fc96ccc1b3df9068ab54f5b1dda
khayrul <orion.beta.ori@gmail.com>
2021-10-26 21:39:31 +0600

    style is updated.

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,46 +1,4 @@
-import { StyleSheet } from 'react-native';
-import colors from './colors';
-
-const buttonStyle = {
-  alignItems: 'center',
-  borderRadius: 4,
-  minWidth: 50,
-  paddingHorizontal: 5,
-  paddingVertical: 3,
-};
-
-export default StyleSheet.create({
-  btnDanger: {
-    ...buttonStyle,
-    backgroundColor: colors.danger
-  },
-
-  btnPrimary: {
-    ...buttonStyle,
-    backgroundColor: colors.primary
-  },
-
-  btnSuccess: {
-    ...buttonStyle,
-    backgroundColor: colors.success
-  },
-  row: {
-    flexDirection: 'row'
-  },
-
-  selfCenter: {
-    alignSelf: 'center'
-  },
-
-  selfEnd: {
-    alignSelf: 'flex-end'
-  },
-
-  selfStart: {
-    alignSelf: 'flex-start'
-  },
-
-  flex1: { flex: 1 },
+export const margin = {
   m5: { margin: 5 },
   m10: { margin: 10 },
   m15: { margin: 15 },
@@ -50,51 +8,70 @@ export default StyleSheet.create({
   ml15: { marginLeft: 15 },
   ml20: { marginLeft: 20 },
   mr5: { marginRight: 5 },
-  mr10: {marginRight: 10},
-  mr15: {marginRight: 15},
-  mr20: {marginRight: 20},
-  mt5: {marginTop: 5},
-  mt10: {marginTop: 10},
-  mt15: {marginTop: 15},
-  mt20: {marginTop: 20},
-  mb5: {marginBottom: 5},
-  mb10: {marginBottom: 10},
-  mb15: {marginBottom: 15},
-  mb20: {marginBottom: 20},
-  mh5: {marginHorizontal: 5},
-  mh10: {marginHorizontal: 10},
-  mh15: {marginHorizontal: 15},
-  mh20: {marginHorizontal: 20},
-  mv5: {marginVertical: 5},
-  mv10: {marginVertical: 10},
-  mv15: {marginVertical: 15},
-  mv20: {marginVertical: 20},
-  p5: { margin: 5 },
-  p10: { margin: 10 },
-  p15: { margin: 15 },
-  p20: { margin: 20 },
-  pl5: { marginLeft: 5 },
-  pl10: { marginLeft: 10 },
-  pl15: { marginLeft: 15 },
-  pl20: { marginLeft: 20 },
-  pr5: {marginRight: 5},
-  pr10: {marginRight: 10},
-  pr15: {marginRight: 15},
-  pr20: {marginRight: 20},
-  pt5: {marginTop: 5},
-  pt10: {marginTop: 10},
-  pt15: {marginTop: 15},
-  pt20: {marginTop: 20},
-  pb5: {marginBottom: 5},
-  pb10: {marginBottom: 10},
-  pb15: {marginBottom: 15},
-  pb20: {marginBottom: 20},
-  ph5: {marginHorizontal: 5},
-  ph10: {marginHorizontal: 10},
-  ph15: {marginHorizontal: 15},
-  ph20: {marginHorizontal: 20},
-  pv5: {marginVertical: 5},
-  pv10: {marginVertical: 10},
-  pv15: {marginVertical: 15},
-  pv20: {marginVertical: 20},
-})
+  mr10: { marginRight: 10 },
+  mr15: { marginRight: 15 },
+  mr20: { marginRight: 20 },
+  mt5: { marginTop: 5 },
+  mt10: { marginTop: 10 },
+  mt15: { marginTop: 15 },
+  mt20: { marginTop: 20 },
+  mb5: { marginBottom: 5 },
+  mb10: { marginBottom: 10 },
+  mb15: { marginBottom: 15 },
+  mb20: { marginBottom: 20 },
+  mh5: { marginHorizontal: 5 },
+  mh10: { marginHorizontal: 10 },
+  mh15: { marginHorizontal: 15 },
+  mh20: { marginHorizontal: 20 },
+  mv5: { marginVertical: 5 },
+  mv10: { marginVertical: 10 },
+  mv15: { marginVertical: 15 },
+  mv20: { marginVertical: 20 },
+};
+
+export const padding = {
+  p5: { padding: 5 },
+  p10: { padding: 10 },
+  p15: { padding: 15 },
+  p20: { padding: 20 },
+  pl5: { paddingLeft: 5 },
+  pl10: { paddingLeft: 10 },
+  pl15: { paddingLeft: 15 },
+  pl20: { paddingLeft: 20 },
+  pr5: { paddingRight: 5 },
+  pr10: { paddingRight: 10 },
+  pr15: { paddingRight: 15 },
+  pr20: { paddingRight: 20 },
+  pt5: { paddingTop: 5 },
+  pt10: { paddingTop: 10 },
+  pt15: { paddingTop: 15 },
+  pt20: { paddingTop: 20 },
+  pb5: { paddingBottom: 5 },
+  pb10: { paddingBottom: 10 },
+  pb15: { paddingBottom: 15 },
+  pb20: { paddingBottom: 20 },
+  ph5: { paddingHorizontal: 5 },
+  ph10: { paddingHorizontal: 10 },
+  ph15: { paddingHorizontal: 15 },
+  ph20: { paddingHorizontal: 20 },
+  pv5: { paddingVertical: 5 },
+  pv10: { paddingVertical: 10 },
+  pv15: { paddingVertical: 15 },
+  pv20: { paddingVertical: 20 },
+};
+
+export const row = {
+  flexDirection: 'row',
+};
+
+export const selfCenter = {
+  alignSelf: 'center',
+};
+
+export const flexEnd = {
+  alignSelf: 'flex-end',
+};
+
+export const flexStart = {
+  alignSelf: 'flex-start',
+};
